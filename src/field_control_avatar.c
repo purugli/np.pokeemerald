b05c124feda40efeb097691412ec79e9afcf3bac
Josh <32826900+ShinyDragonHunter@users.noreply.github.com>
2023-09-21 03:47:51 +0100

    Port FRLG metatile attribute handling
    All tilesets have been updated to use the updated handling. Dungeon exit tiles and the puddles in Meteor Falls are now able to generate wild encounters. Redundant metatile behaviours have also been removed.

--- src/field_control_avatar.c
+++ src/field_control_avatar.c
@@ -52,7 +52,7 @@ static const u8 *GetInteractedWaterScript(struct MapPosition *, u8, u8);
 static bool32 TrySetupDiveDownScript(void);
 static bool32 TrySetupDiveEmergeScript(void);
 static bool8 TryStartStepBasedScript(struct MapPosition *, u16, u16);
-static bool8 CheckStandardWildEncounter(u16);
+static bool8 CheckStandardWildEncounter(u32);
 static bool8 TryArrowWarp(struct MapPosition *, u16, u8);
 static bool8 IsWarpMetatileBehavior(u16);
 static bool8 IsArrowWarpMetatileBehavior(u16, u8);
@@ -136,12 +136,14 @@ int ProcessPlayerFieldInput(struct FieldInput *input)
     struct MapPosition position;
     u8 playerDirection;
     u16 metatileBehavior;
+    u32 metatileAttributes;
 
     gSpecialVar_LastTalked = 0;
     gSelectedObjectEvent = 0;
 
     playerDirection = GetPlayerFacingDirection();
     GetPlayerPosition(&position);
+    metatileAttributes = MapGridGetMetatileAttributeAt(position.x, position.y, METATILE_ATTRIBUTES_ALL);
     metatileBehavior = MapGridGetMetatileBehaviorAt(position.x, position.y);
 
     if (CheckForTrainersWantingBattle() == TRUE)
@@ -159,7 +161,7 @@ int ProcessPlayerFieldInput(struct FieldInput *input)
         if (TryStartStepBasedScript(&position, metatileBehavior, playerDirection) == TRUE)
             return TRUE;
     }
-    if (input->checkStandardWildEncounter && CheckStandardWildEncounter(metatileBehavior) == TRUE)
+    if (input->checkStandardWildEncounter && CheckStandardWildEncounter(metatileAttributes) == TRUE)
         return TRUE;
     if (input->heldDirection && input->dpadDirection == playerDirection)
     {
@@ -666,8 +668,9 @@ void RestartWildEncounterImmunitySteps(void)
     sWildEncounterImmunitySteps = 0;
 }
 
-static bool8 CheckStandardWildEncounter(u16 metatileBehavior)
+static bool8 CheckStandardWildEncounter(u32 currMetatileAttrs)
 {
+    u32 metatileBehavior = ExtractMetatileAttribute(currMetatileAttrs, METATILE_ATTRIBUTE_BEHAVIOR);
     if (sWildEncounterImmunitySteps < 4)
     {
         sWildEncounterImmunitySteps++;
@@ -675,7 +678,7 @@ static bool8 CheckStandardWildEncounter(u16 metatileBehavior)
         return FALSE;
     }
 
-    if (StandardWildEncounter(metatileBehavior, sPrevMetatileBehavior) == TRUE)
+    if (StandardWildEncounter(currMetatileAttrs, sPrevMetatileBehavior) == TRUE)
     {
         sWildEncounterImmunitySteps = 0;
         sPrevMetatileBehavior = metatileBehavior;
